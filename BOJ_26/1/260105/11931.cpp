7f7de7ed630f13c2a41af9912ab140382dec3ba5
atmigtnca <goodmorningyb@gmail.com>
2026-01-05 01:16:20 +0900

    260105

---
+++ BOJ_26/1/260105/11931.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+using ll = long long;
+int ipt[1000001];
+int n;
+
+void input()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ipt[i];
+    }
+}
+
+void output()
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << ipt[i] << '\n';
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    sort(&ipt[0], &ipt[n], [](int a, int b) { return a > b; });
+    output();
+
+    return 0;
+}
